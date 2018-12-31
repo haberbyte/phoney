c1b6396ed89303f8343466ab41240c6ad9e1501b
Jan Habermann <jan@habermann.io>
2018-12-31 11:59:43 +0100

    Use a proper verrsion after 0.1.x

--- lib/phoney/version.rb
+++ lib/phoney/version.rb
@@ -1,3 +1,3 @@
 module Phoney
-  VERSION = "0.9.9"
+  VERSION = "0.2.0"
 end
--- test/regions/us_test.rb
+++ test/regions/us_test.rb
@@ -4,13 +4,13 @@ class USRegionTest < MiniTest::Unit::TestCase
   def setup
     Phoney.region = :us
   end
-  
+
   def test_format_phone_number
     assert_equal "568-9780", Phoney::Parser.parse("5689780")
     assert_equal "(704) 568-9780", Phoney::Parser.parse("7045689780")
     assert_equal "1 (704) 568-9780", Phoney::Parser.parse("17045689780")
   end
-  
+
   def test_as_you_type_formatting
     assert_equal "+1 (7  )", Phoney::Parser.parse("+17")
     assert_equal "+1 (71 )", Phoney::Parser.parse("+171")
@@ -19,15 +19,15 @@ class USRegionTest < MiniTest::Unit::TestCase
     assert_equal "+1 (712) 345", Phoney::Parser.parse("+1712345")
     assert_equal "+1 (712) 345-6", Phoney::Parser.parse("+17123456")
   end
-  
+
   def test_international_incomplete_number_with_trunk_prefix
     assert_equal "+1 (1) (7  )", Phoney::Parser.parse("+117")
   end
-  
+
   def test_asterisk_pattern
     assert_equal "*22812", Phoney::Parser.parse("*22812")
     assert_equal "*22812", Phoney::Parser.parse("*22812")
-    
+
     assert_equal "*12 345", Phoney::Parser.parse("*12345")
     assert_equal "*12 345-6789", Phoney::Parser.parse("*123456789")
     assert_equal "*123 456-7891", Phoney::Parser.parse("*1234567891")
@@ -35,7 +35,7 @@ class USRegionTest < MiniTest::Unit::TestCase
     assert_equal "*123 (456) 789-1234", Phoney::Parser.parse("*1234567891234")
     assert_equal "*12 345678912345", Phoney::Parser.parse("*12345678912345")
   end
-  
+
   def test_fallback_for_invalid_phone_number
     # the number is too long for [:us]
     assert_equal "+1 704123456789", Phoney::Parser.parse("+1704123456789")
