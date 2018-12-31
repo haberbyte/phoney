53a02331622276f11d4477f9d86850b84b5fd115
Jan Habermann <jan@habermann.io>
2018-12-31 12:26:56 +0100

    Adjust README to reflect what the current version does

--- lib/phoney.rb
+++ lib/phoney.rb
@@ -8,8 +8,12 @@ module Phoney
   PLACEHOLDER_CHAR = '#'
   DIGITS           = '0123456789'
   NUMPAD_CHARS     = '+#*'+DIGITS
-  
+
   class << self
+    def format(input, options = {})
+      Phoney::Parser.parse(input, options)
+    end
+
     def region
       @region ||= Region[:us]
     end
@@ -29,76 +33,11 @@ module Phoney
     def area_code=(area_code)
       @area_code = area_code
     end
-    
+
     def version
       VERSION::STRING
     end
   end
-
-  def initialize(params, region_code=nil)
-    region       = Region.find(region_code)
-    country_code = region.country_code.to_s if region
-    
-    if params.is_a?(String)
-      params = Parser.parse_to_parts(params, region_code)
-    end 
-
-    self.number = params[:number].to_s
-    # The rare case when some digits are in front of the area code
-    self.prefix_code = params[:prefix_code].to_s
-    # Can be empty, because some special numbers just don't have an area code (e.g. 911)
-    self.area_code = params[:area_code].to_s || self.class.area_code
-    self.country_code = params[:country_code].to_s || country_code || self.class.country_code
-    
-    raise "Must enter number" if(self.number.nil? || self.number.empty?)
-    raise "Must enter country code or set default country code" if(self.country_code.nil? || self.country_code.empty?)
-  end
-
-  # Does this number belong to the default country code?
-  def has_default_country_code?
-    country_code.to_s == self.class.country_code.to_s
-  end
-
-  # Does this number belong to the default area code?
-  def has_default_area_code?
-    (!area_code.to_s.empty? && area_code.to_s == self.class.area_code.to_s)
-  end
-  
-  # Formats the phone number.
-  # If the method argument is a String, it is used as a format string, with the following fields being interpolated:
-  #
-  # * %c - country_code (385)
-  # * %a - area_code (91)
-  # * %n - number (5125486)
-  #
-  # If the method argument is a Symbol, we use one of the default formattings and let the parser do the rest.
-  def format(fmt)
-    if fmt.is_a?(Symbol)
-      case fmt
-      when :default
-        Parser::parse("+#{country_code} #{prefix_code}#{area_code} #{number}", country_code)
-      when :national
-        Parser::parse("#{area_code} #{number}", country_code)
-      when :local
-        STDERR.puts "Warning: Using local format without setting a default area code!?" if Phoney.area_code.nil?
-        Parser::parse(number, country_code)
-      else
-        raise "The format #{fmt} doesn't exist'"
-      end
-    else
-      format_number(fmt)
-    end
-  end
-
-  # The default format is the canonical format: "+{country_code} {area_code} {number}"
-  def to_s
-    format(:default)
-  end
-  
-  private
-    def format_number(fmt)
-      fmt.gsub("%c", country_code || "").gsub("%a", area_code || "").gsub("%n", number || "")
-    end
 end
 
 # Load our region file when we require the library
